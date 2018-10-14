eb4383b198e3f5285a9740aa0b9ffe5c0a700476
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-10-14 21:10:51 +0200

    add the Page

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Meta from "./Meta";
+import Header from "./Header";
+
+class Page extends React.Component {
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Page;
