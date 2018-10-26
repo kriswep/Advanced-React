ea7a8283980cbcc037363bc50f147f1788c8fced
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-10-26 22:32:19 +0200

    kickoff yoga 🧘 server

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,15 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FROINTEDN_URL,
+    },
+  },
+  deets => console.log(`Server running on http://localhost:${deets.port}`),
+);
