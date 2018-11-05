77e494de1ce7cba2ba17f0577881c3d60c5ead1b
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-11-05 21:08:07 +0100

    create items

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -34,7 +34,7 @@ class Item extends Component {
         <div className="buttonList">
           <Link
             href={{
-              pathname: 'update',
+              pathname: '/update',
               query: { id: item.id },
             }}
           >
