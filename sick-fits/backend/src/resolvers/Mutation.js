d267b016fa694bb909bb8ad3618cef2c0865e340
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-11-07 20:16:39 +0100

    query update items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -13,6 +13,22 @@ const Mutations = {
 
     return item;
   },
+  async updateItem(parent, args, ctx, info) {
+    // copy the updated item
+    const updates = { ...args };
+    // no id
+    delete updates.id;
+    // update
+    const item = await ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: { id: args.id },
+      },
+      info,
+    );
+
+    return item;
+  },
 };
 
 module.exports = Mutations;
