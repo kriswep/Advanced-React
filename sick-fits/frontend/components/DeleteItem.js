af39dda6e6750bd5a081444c382cdccc0eed1847
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-11-08 20:54:10 +0100

    delete items from frontend

---
+++ sick-fits/frontend/components/DeleteItem.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import { ALL_ITEMS_QUERY } from './Items';
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`;
+
+class DeleteItem extends Component {
+  update = (cache, payload) => {
+    // manually update the cache
+    // read the items from the cache
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    // filter deleted item
+    data.items = data.items.filter(
+      item => item.id !== payload.data.deleteItem.id,
+    );
+    // put items back
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
+
+  render() {
+    return (
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => (
+          <button
+            onClick={() => {
+              if (confirm('Do you really want to delete this item?')) {
+                deleteItem();
+              }
+            }}
+          >
+            {this.props.children}
+          </button>
+        )}
+      </Mutation>
+    );
+  }
+}
+
+export default DeleteItem;
+export { DELETE_ITEM_MUTATION };
